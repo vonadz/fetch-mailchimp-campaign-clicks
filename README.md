384475d763e616ae880233daba793ebd144b9a8f
Alex <alex.zdanov@gmail.com>
2022-05-21 15:25:46 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#What it does?
+Fetches all campaigns and URL clicks for a specific Mailchimp list and writes the data to a JSON file.
+
+#What do you need?
+Copy example.env to .env and fill out the variables with your API + server, which you can find in your Mailchimp account.
+Only tested with Node.js 18.2.0
+
+#How to use?
+Clone the repo, run `npm install` in the root directory, run `node index.mjs`, follow instructions in terminal. 
---
+++ index.mjs
@@ -0,0 +1,65 @@
+import 'dotenv/config';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+import client from '@mailchimp/mailchimp_marketing';
+
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+
+const rl = readline.createInterface({ input, output });
+
+const { MAILCHIMP_API, MAILCHIMP_SERVER } = process.env;
+
+if (!MAILCHIMP_API || !MAILCHIMP_SERVER) {
+	throw Error('Make sure API and server are set in .env, check example.env for info');
+}
+
+client.setConfig({
+	apiKey: MAILCHIMP_API,
+  server: MAILCHIMP_SERVER, 
+});
+
+const getDesiredListId = async (allLists) => {
+	const listId = await rl.question('What list to get campaign data for? Use the id from one of the lists output above, without quotation marks.\n');
+	if (!allLists.find(l => l.id === listId)) {
+		console.log(`The id you input doesn't match any in the list. Try again.`);
+		return getDesiredListId(allLists);
+	} else {
+		return listId;
+	}
+};
+
+
+
+
+(async() => {
+
+	try {
+	console.log('Getting all lists.');
+	const allLists = await client.lists.getAllLists();
+	console.log('Retrieved these lists: ', allLists.lists);
+	const desiredListId = await getDesiredListId(allLists.lists);
+	console.log(`Getting all campaigns for list id ${desiredListId} (limited to 1000)`);
+  const allCampaigns = await client.campaigns.list({list_id: desiredListId, count: 1000});	
+		console.log(`Fetched ${allCampaigns.campaigns.length} campaigns.`);
+		console.log('Fetching URL clicks for each campaign.');
+		let count = 0;
+		for (const campaign of allCampaigns.campaigns) {
+			const clickReport = await client.reports.getCampaignClickDetails(campaign.id);
+			campaign.urlClicks = clickReport.urls_clicked;
+			count++;
+			process.stdout.write(`\u001b[2K\u001b[0EFetched ${count}/${allCampaigns.campaigns.length} URL clicks.`)	
+		}
+		console.log('\nDone fetching URL clicks!');
+		console.log('Writing all campaigns + clicks to file.');
+		fs.writeFileSync(path.join(__dirname, `campaigns-urls-${Date.now()}.json`), JSON.stringify(allCampaigns.campaigns));
+		console.log(`Data written to campaigns-urls-${Date.now()}.json in src directory.`);
+	} catch (e) {
+		throw e;
+	} finally {
+		rl.close();
+	}
+})();
